24bfbed87520db4beb842885c8aaa31f795656d0
Vishal Singh <vishal@roposo.com>
2016-07-22 11:57:25 +0530

    Adding build script and adding installation directory to gitignore
    Adding build script and adding installation directory to gitignore

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./autogen.sh
+export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/lib -L/usr/local/opt/expat/lib"
+export CFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+export CPPFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+./configure --prefix=$PWD/installed --enable-mysql
+make && make install
