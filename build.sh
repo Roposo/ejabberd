0bc81eb13c91354be5dfddb077a7796e443f30b6
Vishal Singh <vishal@roposo.com>
2016-07-25 12:27:39 +0530

    Updating build script for Linux systems
    Updating builg script for Linux systems.

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 ./autogen.sh
-export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/lib -L/usr/local/opt/expat/lib"
-export CFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
-export CPPFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Configuring for Mac"
+  export LDFLAGS="-L/usr/local/opt/openssl/lib -L/usr/local/lib -L/usr/local/opt/expat/lib"
+  export CFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+  export CPPFLAGS="-I/usr/local/opt/openssl/include/ -I/usr/local/include -I/usr/local/opt/expat/include"
+elif [ "$(uname)" == "Linux" ]; then
+  echo "Configuring for Linux"
+fi
 ./configure --prefix=$PWD/installed --enable-mysql
 make && make install
